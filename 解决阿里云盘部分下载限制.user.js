9e9f064c7aa0fe54552f9026b1ae1ed3e1c2edfc
Womsxd <45663319+Womsxd@users.noreply.github.com>
2022-08-09 17:48:56 +0800

    Update 解决阿里云盘部分下载限制.user.js

--- 解决阿里云盘部分下载限制.user.js
+++ 解决阿里云盘部分下载限制.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         解决阿里云盘部分下载限制
-// @version      0.1
+// @version      0.2
 // @description  下载文件大小和文件数量都改为Infinity
 // @author       Womsxd
 // @run-at       document-body
+// @match        https://www.aliyundrive.com/drive
 // @match        https://www.aliyundrive.com/drive/*
 // @icon         https://gw.alicdn.com/imgextra/i3/O1CN01aj9rdD1GS0E8io11t_!!6000000000620-73-tps-16-16.ico
 // @grant        none
